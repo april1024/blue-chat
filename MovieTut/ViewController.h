e05745d8e8c15e0570bf0b05298c4c4fceea033e
Kyoungtaek Koo <boxersb@FlyingDutchman.local>
2012-07-18 16:38:25 +0900

    Initial Commit

---
+++ MovieTut/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MovieTut
+//
+//  Created by 경택 구 on 12. 7. 18..
+//  Copyright (c) 2012년 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
